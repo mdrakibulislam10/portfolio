089ed02845c9b561c3b94e99e8b87ada392462dd
mdrakibulislam10 <mdrakibulislam752@gmail.com>
2023-08-08 06:34:22 +0600

    banner updated

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,5 +1,5 @@
 // import bannerImg from "../../../assets/qlq3d339wrt.jpeg-removebg-preview.png";
-import bannerImg from "../../../assets/bannerImg.png"
+import bannerImg from "../../../assets/myBannerImg.png"
 import bottomArrow from "../../../assets/Arrows_down_animated.gif";
 import { TypeAnimation } from 'react-type-animation';
 import { FaFacebook, FaGithubSquare, FaInstagramSquare, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
@@ -8,20 +8,18 @@ const Banner = () => {
     return (
         <section className="mb-20 mt-6 bg-gray-100 py-12" id="home">
             <div className="flex flex-col lg:flex-row justify-center items-center mx-1 lg:mx-20 gap-5 mb-10">
-                <div className="lg:w-1/2 mx-auto">
+                <div className="lg:w-1/2 mx-auto order-2 md:order-1">
                     <div className="flex flex-col h-full justify-center">
                         <h4 className="text-lg md:text-3xl font-semibold">Hi, I'm</h4>
                         <h2 className="text-3xl md:text-6xl my-5 font-semibold">Md. Rakibul Islam</h2>
                         <TypeAnimation
                             className="text-lg md:text-3xl font-semibold"
                             sequence={[
-                                "Web Developer",
+                                "MERN Stack Developer",
                                 1000,
                                 "React.js Developer",
                                 1000,
-                                "Front End Developer",
-                                1000,
-                                "MERN Stack Developer",
+                                "Front-End Developer",
                                 1000,
                                 () => {
                                     // console.log('Sequence completed');
@@ -41,11 +39,11 @@ const Banner = () => {
                     </div>
                 </div>
 
-                <div className="lg:w-1/2 mx-1 md:flex gap-5">
-                    <img className="mx-auto" src={bannerImg} alt="" />
+                <div className="lg:w-1/2 mx-1 md:flex gap-5 order-1 md:order-2">
+                    <img className="mx-auto w-3/4" src={bannerImg} alt="" />
 
                     <div className="flex md:flex-col flex-wrap gap-8 justify-center md:justify-start my-5 md:my-1">
-                        <a href="https://www.linkedin.com/in/md-rakibul-islam-a7a668283/" target="_blank" rel="noreferrer" title="Linkedin"><FaLinkedin className="w-8 h-8" /></a>
+                        <a href="https://www.linkedin.com/in/md-rakibul-islam-dev/" className="md:mt-20" target="_blank" rel="noreferrer" title="Linkedin"><FaLinkedin className="w-8 h-8" /></a>
                         <a href="https://github.com/mdrakibulislam10" target="_blank" rel="noreferrer" title="GitHub"><FaGithubSquare className="w-8 h-8" /></a>
                         <a href="https://www.instagram.com/____rokib____/?hl=en" target="_blank" rel="noreferrer" title="Instagram"><FaInstagramSquare className="w-8 h-8" /></a>
                         <a href="https://twitter.com/Rokib74664686" target="_blank" rel="noreferrer" title="Twitter"><FaTwitterSquare className="w-8 h-8" /></a>
